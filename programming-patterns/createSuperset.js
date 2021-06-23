4e08223519afc6294e75f23d4767b65f5dc553f5
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-06-23 15:41:45 -0400

    additional sliding window algos

--- programming-patterns/createSuperset.js
+++ programming-patterns/createSuperset.js
@@ -5,11 +5,14 @@ function superSet(arr) {
     let sets = [[]];
   
     // generate the power set and for each new set
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = 0, len = sets.length; j < len; j++) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0, len = sets.length; j < len; j++) {
       let tempArr = sets[j].concat(arr[i]);
       sets.push(tempArr);
     }
   }
-  
+
+  return sets
 }
+
+console.log(superSet([1,2,3]))
